3094b3b7d03b5ed0d1346a17079610ecfb3f7ad6
Harmanpreet Kaur <harmanpreetkaur@Harmanpreets-MacBook-Pro.local>
2019-12-09 10:44:01 -0500

    creating new db

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/GamerLobby'
+    //db: 'mongodb://localhost:27017/GamerLobby'
+   db: 'mongodb+srv://harman2:Mongodb16@cluster0-hyur7.mongodb.net/GamerLobby?retryWrites=true'
   };
