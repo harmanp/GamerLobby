4711aac1d3e3a9fab3369ae0ad48be380462734d
Harmanpreet Kaur <harmanpreetkaur@Harmanpreets-MacBook-Pro.local>
2019-12-09 11:17:39 -0500

    changed link

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //db: 'mongodb://localhost:27017/GamerLobby'
-   db: 'mongodb+srv://harman2:Mongodb16@cluster0-hyur7.mongodb.net/GamerLobby?retryWrites=true'
+    db: 'mongodb://localhost:27017/GamerLobby'
+   //db: 'mongodb+srv://harman2:Mongodb16@cluster0-hyur7.mongodb.net/GamerLobby?retryWrites=true'
   };
